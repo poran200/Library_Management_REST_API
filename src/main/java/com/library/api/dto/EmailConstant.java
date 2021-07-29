6ff9941078850535aaa9e46a2c2a6665062191fa
poran200 <chowdury659@gmail.com>
2021-07-29 23:05:10 +0600

    email constant

--- src/main/java/com/library/api/dto/EmailConstant.java
+++ src/main/java/com/library/api/dto/EmailConstant.java
@@ -1,5 +1,5 @@
 package com.library.api.dto;/*
- * @created 7/23/2021
+ * @created 7/29/2021
  *
  * @Author Poran chowdury
  */
@@ -7,18 +7,8 @@ package com.library.api.dto;/*
 public final class EmailConstant {
     private EmailConstant() {
     }
-     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smpts";
-    public static final String USERNAME = "userportal0@gmail.com";
-    public static final String PASSWORD = "poran258";
     public static final String FROM_EMAIL = "user@support.com";
-    public static final String CC_EMAIL = "";
     public static final String EMAIL_SUBJECT = "User Support,LLC - New Password";
-    public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
-    public static final String SMTP_HOST = "mail.smtp.host";
-    public static final String SMTP_AUTH = "mail.smtp.auth";
-    public static final String SMTP_PORT = "mail.smtp.port";
-    public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
-    public static final String SMTP_STARTTLS_REQUIRED = "mail.smtp.starttls.required";
-    public static final int DEFAULT_PORT = 456;
+
 
 }
