a1e473fffebc46f74bafb3cbe9a4258f535d60cc
poran200 <chowdury659@gmail.com>
2021-07-31 03:49:28 +0600

    user admin save when app start and postgres data base config

--- src/main/java/com/library/api/LibraryManagementRestApiApplication.java
+++ src/main/java/com/library/api/LibraryManagementRestApiApplication.java
@@ -1,14 +1,35 @@
 package com.library.api;
 
+import com.library.api.model.Role;
+import com.library.api.model.User;
+import com.library.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class LibraryManagementRestApiApplication {
-
+public class LibraryManagementRestApiApplication implements CommandLineRunner {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
     public static void main(String[] args) {
         SpringApplication.run(LibraryManagementRestApiApplication.class, args);
 
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        User admin = new User(null, "hm", "Hasan", "chowdury",
+                "hasan@gmail.com", "abc", "01757414897",
+                Role.ROLE_LIBRARIAN.name(), null, null, true, true);
+        String encode = passwordEncoder.encode(admin.getPassword());
+        admin.setPassword(encode);
+        if (!userRepository.findByUsername(admin.getUsername()).isPresent()){
+            userRepository.save(admin);
+        }
+
+    }
 }
--- src/test/java/com/library/api/repository/UserRepositoryTest.java
+++ src/test/java/com/library/api/repository/UserRepositoryTest.java
@@ -1,16 +1,8 @@
 package com.library.api.repository;
 
-import com.library.api.model.Role;
-import com.library.api.model.User;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /*
  * @created 7/29/2021
@@ -21,30 +13,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(value = false)
 class UserRepositoryTest {
-    @Autowired
-    UserRepository userRepository;
 
-    @Test
-    @Order(1)
-    void createUser() {
-        User student = new User(null, "2017100000028", "poran", "chowdury",
-                "chowdury@gmail.com", "password", "01757414897",
-                Role.ROLE_STUDENT.name(), "CSE", "46th", true, true);
-        User admin = new User(null, "hm", "Hasan", "chowdury",
-                "hasan@gmail.com", "password", "01757414897",
-                Role.ROLE_LIBRARIAN.name(), null, null, true, true);
-        userRepository.save(student);
-        userRepository.save(admin);
-    }
 
-    @Test
-    @Transactional
-    void getUserFinByIdTest() {
-       assertEquals(userRepository.findById(1L).get().getBorrowBooks().size(),1);
-    }
-
-    @Test
-    void deleteUserById() {
-        userRepository.deleteById(1L);
-    }
 }
