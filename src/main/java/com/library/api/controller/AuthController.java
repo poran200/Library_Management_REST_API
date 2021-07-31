55efc8a6aaa39a26bfd102b2d6671dbc291e4a04
poran200 <chowdury659@gmail.com>
2021-07-31 15:55:35 +0600

    documentation #1

--- src/main/java/com/library/api/controller/AuthController.java
+++ src/main/java/com/library/api/controller/AuthController.java
@@ -5,6 +5,7 @@ package com.library.api.controller;/*
  */
 
 import com.library.api.advice.GlobalControllerAdvice;
+import com.library.api.config.IsLibrarian;
 import com.library.api.dto.LibrarianRegistrationDto;
 import com.library.api.dto.LoginDto;
 import com.library.api.dto.Response;
@@ -30,24 +31,51 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
-@Tag(name = "User Registration and Log in  Rest API ",description = "for send request need to be login as admin or librarian on" +
+@Tag(name = "User Registration and Log in  Rest API ", description = "for send request need to be login as admin or librarian on" +
         " registration user password send to there email address ")
 public class AuthController extends GlobalControllerAdvice {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
 
+    /**
+     * Registration end point to add a user .Require Librarian access
+     * Send password the provided email then user able to log in
+     *
+     * @param dto Student registration dto require property for registration
+     * @return Registered student
+     * @throws ResourceExistException if user email or user name is exits
+     */
+    @IsLibrarian
     @PostMapping("/registration/student")
     public ResponseEntity<Response> registerStudent(@Validated @RequestBody StudentRegistrationDto dto) throws ResourceExistException {
         Response response = userService.register(dto);
         return ResponseEntity.status((int) response.getStatusCode()).body(response);
     }
+
+    /**
+     * Registration end point to add a user .Require Librarian access
+     * Send password the provided email then user able to log in
+     *
+     * @param dto Librarian registration dto require property for registration
+     * @return Registered student
+     * @throws ResourceExistException if user email or user name is exits
+     */
+    @IsLibrarian
     @PostMapping("/registration/liberian")
     public ResponseEntity<Response> registerLiberian(@Validated @RequestBody LibrarianRegistrationDto dto) throws ResourceExistException {
         Response response = userService.register(dto);
         return ResponseEntity.status((int) response.getStatusCode()).body(response);
     }
+
+    /**
+     * Log in rest end point
+     *
+     * @param dto username and password need provide
+     * @return the logged in user information
+     * @throws ResourceNotFoundException if user is not found
+     */
     @PostMapping("/login")
-    public ResponseEntity<Response> login(@Validated @RequestBody LoginDto dto , HttpServletRequest request) throws ResourceNotFoundException {
+    public ResponseEntity<Response> login(@Validated @RequestBody LoginDto dto, HttpServletRequest request) throws ResourceNotFoundException {
         Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authenticate);
         return ResponseEntity.ok().body(userService.findByUserName(authenticate.getName()));
--- src/main/java/com/library/api/controller/BooKRequestController.java
+++ src/main/java/com/library/api/controller/BooKRequestController.java
@@ -9,6 +9,7 @@ import com.library.api.config.IsStudentRole;
 import com.library.api.dto.BookRequestCreateDto;
 import com.library.api.dto.BookRequestResponseDto;
 import com.library.api.dto.Response;
+import com.library.api.exception.BookRequestNotAcceptException;
 import com.library.api.exception.ResourceNotFoundException;
 import com.library.api.model.UserPrincipal;
 import com.library.api.services.BookRequestService;
@@ -21,50 +22,95 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * All rest end point for book request
+ */
 @RestController
 @RequestMapping("api/v1/bookRequest")
 @RequiredArgsConstructor
 public class BooKRequestController {
     private final BookRequestService bookRequestService;
+
+    /**
+     * Create a book request for add a book to library  by student user role
+     *
+     * @param dto book request create dto
+     * @return the created request
+     * @throws ResourceNotFoundException     if user not found
+     * @throws BookRequestNotAcceptException if user already submit a request and it is pending
+     */
     @PostMapping
     @IsStudentRole
-    public ResponseEntity<Response> create(@Validated @RequestBody BookRequestCreateDto dto) throws ResourceNotFoundException {
+    public ResponseEntity<Response> create(@Validated @RequestBody BookRequestCreateDto dto) throws ResourceNotFoundException, BookRequestNotAcceptException {
         String currentUsername = getCurrentUsername();
-        if (currentUsername ==null){
+        if (currentUsername == null) {
             // for dev env
             currentUsername = "2017100000029";
         }
         Response response = bookRequestService.create(currentUsername, dto);
         return ResponseEntity.status((int) response.getStatusCode()).body(response);
     }
+
+    /**
+     * @param id book request id
+     * @return  response the book request which one is found
+     * @throws ResourceNotFoundException if book request no found
+     */
     @IsLibrarian
     @GetMapping("/{id}")
     public ResponseEntity<Response> findById(@Validated @PathVariable(required = true) long id) throws ResourceNotFoundException {
-         return ResponseEntity.ok().body(bookRequestService.findById(id));
+        return ResponseEntity.ok().body(bookRequestService.findById(id));
     }
+
+    /**
+     * @param studentId require the student id
+     * @return the book request by student
+     * @throws ResourceNotFoundException if book request not found
+     */
     @IsStudentRole
     @GetMapping("/student/{studentId}")
     public ResponseEntity<Response> findById(@Validated @PathVariable(required = true) String studentId) throws ResourceNotFoundException {
         return ResponseEntity.ok().body(bookRequestService.findByStudentId(studentId));
     }
+
+    /**
+     * Find all book requests. Requires The librarian access
+     *
+     * @param pageNo   which page user want
+     * @param pageSize page size user can customize it
+     * @param sortBy   sort by particular field which one user want
+     * @return page of  book request
+     */
     @GetMapping("/requests")
     @IsLibrarian
     public ResponseEntity<Response> findAllRequests(@RequestParam(defaultValue = "0") int pageNo,
                                                     @RequestParam(defaultValue = "20") int pageSize,
-                                                    @RequestParam(defaultValue = "createdAt") String sortBy){
+                                                    @RequestParam(defaultValue = "createdAt") String sortBy) {
         PageRequest pageRequest = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
         return ResponseEntity.ok().body(bookRequestService.finAllRequest(pageRequest));
     }
+
+    /**
+     * Update the book request . Requires The librarian access
+     *
+     * @param dto book request response dto must be include the request id
+     * @return the update book request response
+     * @throws ResourceNotFoundException if book request not found in database
+     */
     @PutMapping
     @IsLibrarian
-   public ResponseEntity<Response> update(@Validated @RequestBody BookRequestResponseDto dto) throws ResourceNotFoundException {
+    public ResponseEntity<Response> update(@Validated @RequestBody BookRequestResponseDto dto) throws ResourceNotFoundException {
         return ResponseEntity.ok().body(bookRequestService.update(dto));
-   }
+    }
+
+    /**
+     * @return current authenticate user name form security context
+     */
     private String getCurrentUsername() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Object principal = authentication.getPrincipal();
-        if (principal instanceof UserPrincipal){
-            UserPrincipal userPrincipal =(UserPrincipal) principal;
+        if (principal instanceof UserPrincipal) {
+            UserPrincipal userPrincipal = (UserPrincipal) principal;
             return userPrincipal.getUsername();
         }
         return null;
